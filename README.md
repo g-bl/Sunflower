6d94f3b8fc3b796a7f641156d1390d928911daf9
Fanny Bouyssou <heyfromguillaume@gmail.com>
2019-07-16 21:58:30 +0200

    + Adding icon + Generic toast and page navigation scripts

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-PhoneGap Sunflower CISSP application
+Sunflower CISSP mobile application
+
+Original Sunflower guide authors:
+Maarten de Frankrijker, CISSP.
+Revised by Christian Reina, CISSP.
+Revised by Steve Warnock
+https://www.sunflower-cissp.com/
+
+Application illustration: Philadelphia Museum of Art
+Application icon made by Freepik from http://www.flaticon.com
+
+Made with:
+PhoneGap https://phonegap.com/
+Onsen UI 2 https://onsen.io/
+Abiro icon generator https://pgicons.abiro.com/
+
+Build steps:
+1. Download ZIP from GitHub
+2. Upload this same file to https://build.phonegap.com
+3. Done
--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,35 @@
+var domainsSubsections = [
+    2,
+    2,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+];
+
 document.addEventListener('init', function (event) {
     var page = event.target;
 
-    if (page.id === 'domain1') {
+    page.querySelector('.domain-title').onclick = function () {
+        ons.notification.toast(
+            document.querySelector('#' + page.id + ' .domain-title').innerHTML,
+            { timeout: 2000, animation: 'fall' });
+    };
 
-        page.querySelector('.domain-title').onclick = function () {
-            ons.notification.toast('Domain 1 - Security and Risk Management', { timeout: 2000, animation: 'fall' });
-        };
+    var idParts = page.id.split("-");
 
-        page.querySelector('#push-button-1-1').onclick = function () {
-            document.querySelector('#domain1-navigator').pushPage('domain1-1.html', { data: { title: 'Concepts (10)' } });
-        };
-        page.querySelector('#push-button-1-2').onclick = function () {
-            document.querySelector('#domain1-navigator').pushPage('domain1-2.html', { data: { title: 'Risk (12)' } });
-        };
+    if (idParts.length === 1) {
 
-    } else if (page.id === 'domain2') {
+        var domainNb = idParts[0].replace("domain", "");
 
-        page.querySelector('.domain-title').onclick = function () {
-            ons.notification.toast('Domain 2 - Asset Security', { timeout: 2000, animation: 'fall' });
-        };
-
-        page.querySelector('#push-button-2-1').onclick = function () {
-            document.querySelector('#domain2-navigator').pushPage('domain2-1.html', { data: { title: 'Information classification (110)' } });
-        };
-        page.querySelector('#push-button-2-2').onclick = function () {
-            document.querySelector('#domain2-navigator').pushPage('domain2-2.html', { data: { title: 'Security policies, standards & guidelines (119)' } });
-        };
-
-    } else if (page.id === 'domain1-1') {
-        page.querySelector('.domain-title').onclick = function () {
-            ons.notification.toast('Domain 1 - Security and Risk Management<br />Concepts (10)', { timeout: 2000, animation: 'fall' });
-        };
-    } else if (page.id === 'domain1-2') {
-        page.querySelector('.domain-title').onclick = function () {
-            ons.notification.toast('Domain 1 - Security and Risk Management<br />Risk (12)', { timeout: 2000, animation: 'fall' });
-        };
-
-    } else if (page.id === 'domain2-1') {
-        page.querySelector('.domain-title').onclick = function () {
-            ons.notification.toast('Domain 2 - Asset Security<br />Information classification (110)', { timeout: 2000, animation: 'fall' });
-        };
-    } else if (page.id === 'domain2-2') {
-        page.querySelector('.domain-title').onclick = function () {
-            ons.notification.toast('Domain 2 - Asset Security<br />Security policies, standards & guidelines (119)', { timeout: 2000, animation: 'fall' });
-        };
+        for (let i = 1; i < domainsSubsections[domainNb - 1] + 1; i++)
+        {
+            page.querySelector('#push-button-' + domainNb + '-' + i).onclick = function () {
+                document.querySelector('#domain' + domainNb + '-navigator').pushPage('domain' + domainNb + '-' + i + '.html', { });
+            };
+        }
     }
 });
 
